e24da0ff635473b62a65c7811bb05bf0a508f4f5
JonnyKong <jonnykong1996@gmail.com>
2019-09-10 19:44:44 -0700

    Make year filter apply to total selected cases

--- tools/sqlserver_get_queries.sql
+++ tools/sqlserver_get_queries.sql
@@ -1,7 +1,3 @@
-SELECT      SQLTEXT.text, STATS.last_execution_time
-FROM        sys.dm_exec_query_stats STATS
-CROSS APPLY sys.dm_exec_sql_text(STATS.sql_handle) AS SQLTEXT
-WHERE       STATS.last_execution_time > GETDATE()-1
-            AND SQLTEXT.text LIKE '%meningitis%'
-ORDER BY    STATS.last_execution_time DESC;
+SELECT *
+FROM data;
 GO
