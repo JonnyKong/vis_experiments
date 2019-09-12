93a36721622569be7a68105eaa61bae12131db3b
JonnyKong <jonnykong1996@gmail.com>
2019-09-11 20:18:04 -0700

    Update tools

--- tools/sqlserver_get_queries.sql
+++ tools/sqlserver_get_queries.sql
@@ -1,4 +1,4 @@
-SELECT      SQLTEXT.text, STATS.last_execution_time
+SELECT      SQLTEXT.text, STATS.last_execution_time, STATS.execution_count
 FROM        sys.dm_exec_query_stats STATS
 CROSS APPLY sys.dm_exec_sql_text(STATS.sql_handle) AS SQLTEXT
 WHERE       STATS.last_execution_time > GETDATE()-1
